ea473a5c6fc8c662dafe224c470b126d6525e6b5
Fabio Teichmann <fabio.teichmann15@gmail.com>
2025-06-25 11:59:02 +0300

    shell script trouble shooting

--- scripts/bootstrap/bastion-startup.sh
+++ scripts/bootstrap/bastion-startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum update -y
-yum install -y curl unzip amazon-ssm-agent --skip-broken
+yum install -y curl unzip amazon-ssm-agent --skip-broken --allowerasing
 
 systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
@@ -18,6 +18,9 @@ sudo ./get_helm.sh
 sudo curl -LO "https://dl.k8s.io/release/$(curl -sL https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" 
 sudo chmod +x kubectl 
 sudo mv kubectl /usr/local/bin/
+
+echo "$AWS_REGION"
+echo "$CLUSTER_NAME"
 
 aws eks update-kubeconfig \
     --region "$AWS_REGION" \
