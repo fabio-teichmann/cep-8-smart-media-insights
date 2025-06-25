8ad39047f8c91b3ae50a91c621b593eba3a2d9a0
Fabio Teichmann <fabio.teichmann15@gmail.com>
2025-06-25 10:45:37 +0300

    changed vpc version

--- terraform/modules/aws_vpc/main.tf
+++ terraform/modules/aws_vpc/main.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "6.0.0"
+  version = "5.21.0"
 
   name = "${var.plat_name}-vpc"
   cidr = "10.0.0.0/16"
