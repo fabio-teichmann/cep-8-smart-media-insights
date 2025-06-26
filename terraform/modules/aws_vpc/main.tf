9f5ed3b13ad5a852f01fa34e9cb8a4a99d6d43f3
Fabio Teichmann <fabio.teichmann15@gmail.com>
2025-06-26 14:24:48 +0300

    opened VPC SG to public cidr blocks for testing

--- terraform/modules/aws_vpc/main.tf
+++ terraform/modules/aws_vpc/main.tf
@@ -42,6 +42,16 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   cidr_ipv4         = each.value
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  for_each = toset(module.vpc.public_subnets_cidr_blocks)
+
+  security_group_id = aws_security_group.vpc_enpoint_sg.id
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_ipv4         = each.value
+}
+
 resource "aws_vpc_security_group_egress_rule" "all_out" {
   security_group_id = aws_security_group.vpc_enpoint_sg.id
   ip_protocol       = "-1"
