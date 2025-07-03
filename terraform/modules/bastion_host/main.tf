10617756ee7b2d5ed4bb7a16ba4f1f1d842b7d8e
Fabio Teichmann <fabio.teichmann15@gmail.com>
2025-07-03 09:26:56 +0300

    cluster admin policy for bastion

--- terraform/modules/bastion_host/main.tf
+++ terraform/modules/bastion_host/main.tf
@@ -53,6 +53,11 @@ resource "aws_iam_role_policy_attachment" "bastion_ssm_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "bastion_eks_admin" {
+  role = aws_iam_role.bastion_ssm_role.name
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+}
+
 # EKS specific IAM requirements to use bastion for app deployment
 data "aws_iam_policy_document" "bastion_eks_policies" {
   statement {
