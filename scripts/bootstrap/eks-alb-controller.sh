fe4e79eb9edd028c355e1d8b8e8d1805d5d4f7f7
Fabio Teichmann <fabio.teichmann15@gmail.com>
2025-07-15 10:57:52 +0300

    changed ALB helm to upgrade -i

--- scripts/bootstrap/eks-alb-controller.sh
+++ scripts/bootstrap/eks-alb-controller.sh
@@ -35,7 +35,7 @@ helm repo add eks https://aws.github.io/eks-charts
 
 helm repo update eks
 
-helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
+helm upgrade --install aws-load-balancer-controller eks/aws-load-balancer-controller \
   -n kube-system \
   --set clusterName="${CLUSTER_NAME}" \
   --set region="${AWS_REGION}" \
